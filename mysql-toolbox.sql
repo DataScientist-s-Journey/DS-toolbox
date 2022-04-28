60839385aeef820f99da11b4ae9550a07c604fef
Adolfo Correa <adolfocorrea@ibm.com>
2022-04-28 20:23:28 -0300

    add temporary tables

--- mysql-toolbox.sql
+++ mysql-toolbox.sql
@@ -270,4 +270,205 @@ group by 1 having count(r.rental_id)<20 order by 3 limit 5;
 # -- 5 rows in set (0.02 sec)
 
 # -- How much revenue has one single store made over PG-13 and R-Rated films?
+# -- = > < >= <= <> !=
 # -- select 
+# -- PG-13 and R-Rated in film.rating Revenue Store_id
+# -- MySQL
+# -- https://dev.mysql.com/doc/refman/8.0/en/system-schema.html#system-schema-data-dictionary-tables
+# -- SELECT COLUMN_NAME,TABLE_NAME AS 'TableName' FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME LIKE '%film%';
+# -- Oracle
+# -- select * from all_columns where column_name like '%nome%'
+
+# -- select f.rating, sum(p.amount) from film f, payment p, inventory i, rental r
+# -- where f.film_id = i.film_id and i.inventory_id = r.inventory_id and r.rental_id=p.rental_id
+# -- group by f.rating limit 5;
+
+# -- select f.rating, sum(p.amount), i.store_id from film f, payment p, inventory i, rental r
+# -- where f.film_id = i.film_id and i.inventory_id = r.inventory_id and r.rental_id=p.rental_id
+# -- group by f.rating, i.store_id order by i.store_id limit 10;
+
+select f.rating, sum(p.amount), i.store_id from film f, payment p, inventory i, rental r
+where f.film_id = i.film_id and i.inventory_id = r.inventory_id and r.rental_id=p.rental_id and f.rating in ("PG-13", "R")
+group by f.rating, i.store_id order by i.store_id limit 10;
+
+# -- +--------+---------------+----------+
+# -- | rating | sum(p.amount) | store_id |
+# -- +--------+---------------+----------+
+# -- | PG-13  |       8091.51 |        1 |
+# -- | R      |       6514.62 |        1 |
+# -- | PG-13  |       7167.65 |        2 |
+# -- | R      |       6755.57 |        2 |
+# -- +--------+---------------+----------+
+# -- 4 rows in set (0.04 sec)
+
+# -- #revenue for store 1 where film is rated R or PG-13
+# -- SELECT
+# -- i. store_id as Store, f. rating as movie_rating, sum(p.
+# -- amount) as store_revenue
+# -- FROM
+# -- film f, payment p, inventory i, rental
+# -- WHERE I
+# -- p. rental_id = r. rental_id
+# -- AND r.invento ry_id = i.invento ry_id
+# -- AND i.film_id = f. film_id
+# -- AND i.Sto re_id = 1
+# -- AND f. rating in( 'R', 'PG-13')
+# -- GROUP BY 1, 2
+# -- ORDER BY 3 desc
+
+select f.rating, sum(p.amount), i.store_id from film f, payment p, inventory i, rental r where f.film_id = i.film_id and i.inventory_id = r.inventory_id and r.rental_id=p.rental_id and f.rating in ("PG-13", "R") and r.rental_date between '2005-06-08' and '2005-07-19' group by f.rating, i.store_id order by i.store_id limit 5;
+
++--------+---------------+----------+
+| rating | sum(p.amount) | store_id |
++--------+---------------+----------+
+| PG-13  |       2946.32 |        1 |
+| R      |       2283.51 |        1 |
+| PG-13  |       2547.74 |        2 |
+| R      |       2467.96 |        2 |
++--------+---------------+----------+
+4 rows in set (0.04 sec)
+
+# -- select r.customer_id, count(distinct r.rental_id) from rental r group by 1 limit 5;
+
+select rpc.customer_id, rpc.rentals, sum(p.amount) 
+from 
+(select r.customer_id, count(distinct r.rental_id) rentals from rental r group by 1) as rpc,
+ payment p
+where p.customer_id = rpc.customer_id group by 1 limit 5;
+
++-------------+---------+---------------+
+| customer_id | rentals | sum(p.amount) |
++-------------+---------+---------------+
+|           1 |      32 |        118.68 |
+|           2 |      27 |        128.73 |
+|           3 |      26 |        135.74 |
+|           4 |      22 |         81.78 |
+|           5 |      38 |        144.62 |
++-------------+---------+---------------+
+5 rows in set (0.04 sec)
+
+select rpc.customer_id, rpc.rentals, sum(p.amount) from (select r.customer_id, count(distinct r.rental_id) rentals from rental r group by 1) as
+rpc, payment p where p.customer_id = rpc.customer_id and rpc.rentals>20 group by 1 limit 5;
+
+
+select rpc.rentals, count(distinct rpc.customer_id), sum(p.amount) from (select r.customer_id, count(distinct r.rental_id) rentals from rental r group by 1) as rpc, payment p where p.customer_id = rpc.customer_id and rpc.rentals>20 group by 1 limit 5;
+
++---------+---------------------------------+---------------+
+| rentals | count(distinct rpc.customer_id) | sum(p.amount) |
++---------+---------------------------------+---------------+
+|      21 |                              28 |       2442.11 |
+|      22 |                              35 |       3288.30 |
+|      23 |                              45 |       4325.65 |
+|      24 |                              36 |       3575.36 |
+|      25 |                              50 |       5223.49 |
++---------+---------------------------------+---------------+
+5 rows in set (0.02 sec)
+
+select rpc.customer_id, rpc.rentals, sum(p.amount) from (select r.customer_id, count(distinct r.rental_id) rentals from rental r group by 1) as rpc, payment p where p.customer_id = rpc.customer_id and rpc.rentals>20 group by 1 order by 3 desc limit 5;
++-------------+---------+---------------+
+| customer_id | rentals | sum(p.amount) |
++-------------+---------+---------------+
+|         526 |      45 |        221.55 |
+|         148 |      46 |        216.54 |
+|         144 |      42 |        195.58 |
+|         178 |      39 |        194.61 |
+|         137 |      39 |        194.61 |
++-------------+---------+---------------+
+5 rows in set (0.03 sec)
+
+# -- Temporary Table
+create temporary table rpc as
+SELECT
+r.customer_id,
+count(distinct r.rental_id) as num_rentals
+FROM
+rental r
+GROUP BY 1;
+
+SELECT
+sum(p.amount) as total_rev
+FROM
+rpc,
+payment p
+WHERE
+rpc.customer_id=p.customer_id
+AND rpc.num_rentals>20;
+
++-----------+
+| total_rev |
++-----------+
+|  63157.64 |
++-----------+
+1 row in set (0.03 sec)
+
+select c.customer_id, a.phone from customer c left join address a on c.address_id = a.address_id where c.active = 1 order by 2 desc limit 5;
+
+select c.*, a.phone from customer c left join address a on c.address_id = a.address_id where c.active = 1 order by 2 desc limit 5;
+
+
+
+create temporary table activeUsers as
+select
+    c.*,
+    a.phone
+from
+    customer c
+        join address a on c.address_id = a.address_id
+where
+    c.active = 1
+group by 1;
+
+select r.customer_id, count(distinct r.rental_id) rentals, max(r.rental_date) from rental r group by 1 having rentals >= 30 limit 5;
+
+
+create temporary table rewardedUsers
+select r.customer_id, count(distinct r.rental_id) rentals, max(r.rental_date) from rental r group by 1 having rentals >= 30;
+
+select a.customer_id, a.email, a.first_name from activeUsers a inner join rewardedUsers r on a.customer_id = r.customer_id limit 5;
+
+select a.customer_id, a.email, a.phone from activeUsers a right join rewardedUsers r on a.customer_id = r.customer_id limit 5;
+
+
+select a.customer_id, a.email, a.phone, a.active from activeUsers a right join rewardedUsers r on a.customer_id = r.customer_id order by 3 desc limit 5;
+
+select c.customer_id, c.email, ad.phone, c.active
+from activeUsers a right join rewardedUsers r on a.customer_id = r.customer_id, customer c, address adwhere a.customer_id=c.customer_id order by 4 limit 5;
+
+
+drop temporary table if exists activeUsers;
+create temporary table activeUsers as
+select
+    c.*,
+    a.phone
+from
+    customer c
+        join address a on c.address_id = a.address_id
+where
+    c.active = 1
+group by 1;
+
+
+drop temporary table if exists rewardUsers;
+create temporary table rewardUsers
+select r.customer_id, count(distinct r.rental_id) rentals, max(r.rental_date) from rental r group by 1 having rentals >= 30;
+
+# -- Brain
+# -- reward users AND ACTIVE users using the J0IN function
+select a.customer_id, a.email, a.first_name from activeUsers a inner join rewardedUsers r on a.customer_id = r.customer_id limit 5;
+
+# -- All reward users information
+select r.customer_id, c.email, a.phone, c.active
+from customer c inner join rewardedUsers r on c.customer_id = r.customer_id left join activeUsers a on a.customer_id=c.customer_id order by 4 limit 5;
+
+select r.customer_id, c.email, a.phone, c.active
+       from customer c inner join rewardedUsers r on c.customer_id = r.customer_id left join activeUsers a on a.customer_id=c.customer_id order by 4 limit 5;
++-------------+----------------------------------+-------+--------+
+| customer_id | email                            | phone | active |
++-------------+----------------------------------+-------+--------+
+|         368 | HARRY.ARCE@sakilacustomer.org    | NULL  |      0 |
+|         406 | NATHAN.RUNYON@sakilacustomer.org | NULL  |      0 |
+|         241 | HEIDI.LARSON@sakilacustomer.org  | NULL  |      0 |
+|         446 | THEODORE.CULP@sakilacustomer.org | NULL  |      0 |
+|          64 | JUDITH.COX@sakilacustomer.org    | NULL  |      0 |
++-------------+----------------------------------+-------+--------+
+5 rows in set (0.01 sec)
